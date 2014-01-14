81aeef4d086143c55d2888c5291fa9622a12e0c5
Timothy W. Hilton <thilton@ucmerced.edu>
2014-01-14 12:20:15 -0800

    - added STEM_vis.coords_to_grid: brute-force translation   of column representation of gridded data (x y data) to   a grid reprentation (nx by ny array)

--- STEM_vis.py
+++ STEM_vis.py
@@ -3,9 +3,17 @@ output netcdf files into Python."""
 
 from datetime import datetime
 import numpy as np
+import numpy.ma as ma
 from netCDF4 import Dataset
 
 import na_map
+
+def coords_to_grid(x_seq, y_seq, data_seq, nx=124, ny=124):
+    data = np.tile(np.array(np.NaN), [nx, ny])
+    for i in range(len(x_seq)):
+        data[x_seq[i]-1, y_seq[i]-1] = data_seq[i]
+
+    return(data)
 
 def parse_STEM_var(nc_fname=None, t0=None, t1=None, varname=None):
     """ Parse a STEM variable from a STEM I/O API netcdf file.
@@ -31,12 +39,11 @@ def parse_STEM_coordinates(topo_fname):
 
     lat = np.squeeze(topo.variables['LAT'])
     lon = np.squeeze(topo.variables['LON'])
+    topo = np.squeeze(topo.variables['TOPO'])
 
-    return(lon, lat)
+    return(lon, lat, topo)
 
 def initialize_STEM_map():
     """Initializes and returns a na_map object with "missing" color
     legend turned off."""
     return(na_map.NAMapFigure(missing_axis=None, cb_axis=True))
-
-
