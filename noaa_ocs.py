fb0e0b4542aff64984da6edb0ebb88ce5b3576fc
Timothy W. Hilton <thilton@ucmerced.edu>
2014-03-23 20:08:06 -0400

    - implemented finding STEM Z level for NOAA observations - improved documentation - several bug fixes

--- noaa_ocs.py
+++ noaa_ocs.py
@@ -9,14 +9,27 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import basemap
 from scipy.spatial import cKDTree
 from brewer2mpl import qualitative as brewer_qualitative
+
+import na_map
 import STEM_parsers
+
 
 class NOAA_OCS(object):
     """
-    Container class to work with NOAA airborne OCS observations
+    Container class to work with NOAA airborne observations
+    Class attributes:
+       obs: pandas DataFrame containing NOAA observations
+       obs_color: matplotlib color to use for plotting observations.  
+       grid_color: matplotlib color to use for plotting STEM grid cell points
     """
     def __init__(self,
                  obs=None):
+        """
+        Class constructor.
+        PARAMETERS
+        ----------
+        obs: Pandas DataFrame containing NOAA airborne observations.
+        """
         self.obs = obs
         self.obs_color = brewer_qualitative.Dark2['max'].mpl_colors[0]
         self.grid_color = brewer_qualitative.Dark2['max'].mpl_colors[1]
@@ -24,10 +37,12 @@ class NOAA_OCS(object):
     @classmethod
     def parse_file(cls, fname):
         """
-        parses a NOAA airborne OCS measurement text file.
+        parses a NOAA airborne measurement text file.
         PARAMETERS
         ----------
-        fname: full path the NOAA OCS file to be parsed
+        fname: full path the NOAA file to be parsed
+
+        returns: object of class noaa_ocs.NOAA_OCS.
         """
 
         # the NOAA files contain varying number of header lines.  It
@@ -49,7 +64,7 @@ class NOAA_OCS(object):
                          skiprows=hdr_count-1,
                          names=names)
         # convert the times to datetime objects
-        df['date'] = df.apply(
+        df['datet'] = df.apply(
             lambda row: datetime(row['sample_year'],
                                  row['sample_month'],
                                  row['sample_day'],
@@ -68,7 +83,7 @@ class NOAA_OCS(object):
         stem_lon: longitudes of STEM grid cell centers
         stem_lat: latitudes of STEM grid cell centers
         """
-        self.obs['x_stem'], self.obs['y_stem'] = get_stem_xy(
+        self.obs['x_stem'], self.obs['y_stem'] = find_nearest_stem_xy(
             self.obs.sample_longitude,
             self.obs.sample_latitude,
             stem_lon,
@@ -76,19 +91,46 @@ class NOAA_OCS(object):
         self.obs['lon_stem'] = stem_lon[self.obs.x_stem, self.obs.y_stem]
         self.obs['lat_stem'] = stem_lat[self.obs.x_stem, self.obs.y_stem]
 
-    def calcT(start, obsT):
+    def get_stem_z(self,
+                   topo_fname='./TOPO-124x124.nc',
+                   wrfheight_fname='./wrfheight-124x124-22levs.nc'):
         """
-        Calculates the stem t. Start is the start of the stem simulation
-        and obsT is the observation time. Both are given as [hour, day,
-        month, year].
+        determine the STEM z level of NOAA airborne observations using
+        each observation's STEM grid x, STEM grid y, and altitude above
+        sea level, and STEM grid sigma levels.
+        PARAMETERS
+        ----------
+        topo_fname: full path the to the IO/API file defining the
+           latitude, longitude, and topopgraphy of the STEM grid
+        wrfheight_fname: full path the to the IO/API file defining the
+           WRF heights on the STEM grid.
         """
-        SECONDS_PER_HOUR = 60*60
-        # convert start, obsT to datetime.datetime
-        start = datetime(start[3], start[2], start[1], start[0], 0, 0)
-        obsTt = datetime(obsT[3], obsT[2], obsT[1], obsT[0], 0, 0)
+        asl = get_STEMZ_height_ASL(topo_fname, wrfheight_fname)
+        n_obs = self.obs.shape[0]
+        self.obs['z_stem'] = np.NaN  #initialize z to NaN
+        #import pdb; pdb.set_trace()
+        for i in range(n_obs):
+            bin_idx = np.digitize(
+                x=self.obs['sample_altitude'].values[np.newaxis, i],
+                bins=asl[...,
+                         self.obs.x_stem.values[i],
+                         self.obs.y_stem.values[i]])
+            #"+1" because python indices are 0-based, STEM indices are 1-based
+            self.obs['z_stem'].values[i] = bin_idx + 1 
 
-        dt = (obsT - start).total_seconds() / SECONDS_PER_HOUR
-        return(dt)
+    def get_stem_t(self, stem_t0):
+        """
+        Calculates the stem timestep in hours from STEM starting time
+        for each observation and places them in field 'stemt' in the
+        DataFrame of observations.
+        PARAMETERS
+        ----------
+        stem_t0: datetime.datetime; the first timestep of the stem
+           simulation.
+        """
+        #lamda function to convert np.timedelta64 to hours
+        td2hrs = lambda x: x / np.timedelta64(1, 'h')
+        self.obs['stemt'] = (self.obs.datet - stem_t0).apply(td2hrs)
 
     def plot_stem_obs_matches(self):
         """
@@ -108,28 +150,33 @@ class NOAA_OCS(object):
         lon_pad = (lon_max - lon_min) * 0.1
 
         # lon, lat for SW corner of map
-        SW_crnr = (lon_min - lon_pad, lat_min - lat_pad) 
+        SW_crnr = (max(lon_min - lon_pad, -180),
+                   max(lat_min - lat_pad, -90)) 
         # lon, lat for NE corner of map
-        NE_crnr = (lon_max + lon_pad, lat_max + lat_pad) 
-        m = basemap.Basemap(llcrnrlon=SW_crnr[0], llcrnrlat=SW_crnr[1],
-                            urcrnrlon=NE_crnr[0], urcrnrlat=NE_crnr[1])
+        NE_crnr = (min(lon_max + lon_pad, 720),
+                   min(lat_max + lat_pad, 90))
 
-        m.drawmeridians(np.linspace(lon_max, lon_min, num=3),
-        			labels=[1,1,1,1])
-        m.drawparallels(np.linspace(lat_max, lat_min, num=3),
-        			labels=[1,1,1,1])
+        m = na_map.NAMapFigure(cb_axis=None).map
+        # m = basemap.Basemap(llcrnrlon=SW_crnr[0], llcrnrlat=SW_crnr[1],
+        #                     urcrnrlon=NE_crnr[0], urcrnrlat=NE_crnr[1])
+        # m.drawmeridians(np.linspace(lon_max, lon_min, num=3),
+        # 			labels=[0,0,0,1])
+        # m.drawparallels(np.linspace(lat_max, lat_min, num=3),
+        # 			labels=[1,0,0,0])
 
-        obs_mrks = m.scatter(self.obs.sample_longitude,
-                             self.obs.sample_latitude,
+        obs_mrks = m.scatter(self.obs.sample_longitude.values,
+                             self.obs.sample_latitude.values,
                              c=self.obs_color,
                              label='observation',
-                             zorder=2)
+                             zorder=2,
+                             latlon=True)
         grid_mrks = m.scatter(self.obs['lon_stem'].values,
                               self.obs['lat_stem'].values,
                               color=self.grid_color,
                               marker='^',
                               label='STEM grid cell',
-                              zorder=3)
+                              zorder=3,
+                              latlon=True)
         n_obs = self.obs.shape[0]
         for i in range(n_obs):
             ln = m.plot((self.obs['lon_stem'].values[i],
@@ -137,24 +184,33 @@ class NOAA_OCS(object):
                         (self.obs['lat_stem'].values[i],
                          self.obs['sample_latitude'].values[i]),
                         color='black',
+                        latlon=True,
                         zorder=1)
         plt.legend(numpoints=1,
                    scatterpoints=1,
                    loc='best')
         return(m)
-        
+
     def plot_locations(self):
+        """
+        Draw a scatter plot on a map of the location of each observation.
+        """        
         m = init_NA_map()
         mrks = m.scatter(
-            self.obs.sample_longitude,
-            self.obs.sample_latitude,
+            self.obs.sample_longitude.values,
+            self.obs.sample_latitude.values,
             c=self.obs_color,
-            label='observation')
+            label='observation',
+            latlon=True)
         return(m, mrks)
 
 def init_NA_map():
+    m = na_map.NAMapFigure(cb_axis=None).map
+    return(m)
+    
+def init_NA_map_cyl():
     """
-    plot data locations on a map of North America
+    Draw a map of North America using a cylindrical projection.
     """
     plt.figure(figsize=(8,8))
     #initialize a map of North America
@@ -163,123 +219,41 @@ def init_NA_map():
     m = basemap.Basemap(llcrnrlon=SW_crnr[0], llcrnrlat=SW_crnr[1],
                         urcrnrlon=NE_crnr[0], urcrnrlat=NE_crnr[1])
     m.drawmeridians(np.linspace(SW_crnr[0], NE_crnr[0], num=3),
-			labels=[1,1,1,1])
+                    labels=[1,0,0,0])
     m.drawparallels(np.linspace(SW_crnr[1], NE_crnr[1], num=3),
-			labels=[1,1,1,1])
+                    labels=[0,0,0,1])
     m.drawcoastlines()
     return(m)
-    
-def get_height_levels(topo_fname='./TOPO-124x124.nc',
-                      wrfheight_fname='./wrfheight-124x124-22levs.nc'):
+
+def get_STEMZ_height_ASL(topo_fname='./TOPO-124x124.nc',
+                         wrfheight_fname='./wrfheight-124x124-22levs.nc'):
     """
-    Reads the wrf height and topo files to get the asl cell height
-    boundaries.
+    Reads the wrf height and topo files to get the STEM vertical cell
+    boundaries in meters above sea level.  How model levels should be
+    interpreted is described in (1) and (2):
+    (1) http://www.ecmwf.int/research/ifsdocs/DYNAMICS/Chap2_Discretization4.html#961180
+    (2) Simmons, A.J., and D.M. Burridge, 1981: An energy and angular
+        momentum conserving vertical finite difference scheme and hybrid
+        vertical coordinates. Mon. Weather Rev., 109, 758-766.
     PARAMETERS
     ----------
     topo_fname: full path to the IO/API file containing lat, lon, and
-    height above sea level for STEM grid cells
+       height above sea level for STEM grid cells
     wrfheight_fname: full path to the IO/API file containing z levels
-    for stem grid cells
+       for stem grid cells
     """
     lon, lat, topo = STEM_parsers.parse_STEM_coordinates(topo_fname)
 
     f = netcdf.netcdf_file(wrfheight_fname, 'r')
-    agl = f.variables['AGL'][0]  #numpy array shape(22, 124, 124): zlev, lat, lon
+    #numpy array shape(22, 124, 124): zlev, lat, lon.  squeeze removes
+    #the time dimension
+    agl = f.variables['AGL'][:].squeeze()  
     f.close()
 
     asl = agl + topo
     return(asl)
 
-def Haversine(p1,p2):
-    """#The Haversine will calculate the distance between two coordinates.
-    Input:
-            p1 (list[lng,lat]) = coordinates of point 1
-            p2 (list[lng,lat]) = coordinates of point 2
-    Output: distance in km
-    """
-
-    # Variable setup
-    lat1 = p1[0]*0.0174532925
-    lat2 = p2[0]*0.0174532925
-    lon1 = p1[1]*0.0174532925
-    lon2 = p2[1]*0.0174532925
-    R = 6371.0#km
-
-    #d=R*math.acos(math.cos((lon1-lon2)*0.0174532925)*math.cos(lat1*0.0174532925)*math.cos(lat2*0.0174532925)+math.sin(lat1*0.0174532925)*math.sin(lat2*0.0174532925))
-
-    dLat = (lat2-lat1)
-    dLon = lon2-lon1
-    a = (math.sin(dLat/2))**2 + math.cos(lat1)*math.cos(lat2)*(math.sin(dLon/2))**2
-    c = 2*math.atan2(math.sqrt(a), math.sqrt(1-a))
-    d = R*c
-    return d
-
-
-def get_zlev(alt,y,x,zlevs):
-	for level in range(22):
-		print level
-		print zlevs[level][y][x]
-		if alt<zlevs[level][y][x]:
-			return level
-
-
-
-
-def main(filename):
-	print filename
-	# get topo data in list from bottom left to top right
-	f = open('points.csv', 'r')
-	topo_pts = []
-	for line in f:
-		temp = line.replace('\n','').split(',')
-		for i in range(len(temp)):
-			temp[i] = float(temp[i])
-		topo_pts.append(temp)
-	f.close()
-
-
-
-	obs_pts =[]
-	data = []
-	extract_obs(data,obs_pts, filename)
-	f = open('data2008_2009.dat','w')
-	f.write(str(len(obs_pts))+'\n')
-	f.write('t, x, y, z, data\n')
-	zlevs = get_height_levels()
-
-
-	for i in range(len(obs_pts)):
-		# Calculate x and y
-		d = 9999999999999999
-		n=0
-		for j in range(len(topo_pts)):
-			distance = Haversine(topo_pts[j],[obs_pts[i][0], obs_pts[i][1]])
-			if distance<d:
-				n = j
-				d = distance
-		if n == 0:
-			y = 0
-		else:
-			y = n/124
-		x = n-y*124
-
-		# Calculate t
-		start = [0,1,1,2008]
-
-
-		t = calcT(start, [obs_pts[i][2],obs_pts[i][3],obs_pts[i][4],obs_pts[i][5]])
-		print t, 'dogfood'
-
-		# Calculate p
-
-		z = get_zlev(obs_pts[i][6],y,x,zlevs)
-
-		# write data and print check.
-		f.write(str(t)+'\t'+str(x)+'\t'+ str(y)+'\t'+str(z)+'\t'+str(data[i])+'\n')
-
-	f.close()
-
-def get_stem_xy(lon, lat, lon_stem, lat_stem):
+def find_nearest_stem_xy(lon, lat, lon_stem, lat_stem):
     """given a set of arbitrary (lon, lat) positions, find the horizontal
     (x, y) STEM grid indices of the nearest STEM grid cell center to
     each position.
@@ -297,9 +271,10 @@ def get_stem_xy(lon, lat, lon_stem, lat_stem):
     x, y, z = lon_lat_to_cartesian(lon, lat)
     xs, ys, zs = lon_lat_to_cartesian(lon_stem, lat_stem)
 
-    #use a KD tree to find the nearest neighbor to (x,y,z) from the
-    #points within (xs, ys, zs).  A KD tree is an data structure that
-    #allows efficient queries of K-dimensional space (K here is 3).
+    #use a K-dimensional tree to find the nearest neighbor to (x,y,z)
+    #from the points within (xs, ys, zs).  A KD tree is a data
+    #structure that allows efficient queries of K-dimensional space (K
+    #here is 3).
     tree = cKDTree(np.dstack((xs.flatten(),
                               ys.flatten(),
                               zs.flatten())).squeeze())
@@ -311,9 +286,12 @@ def get_stem_xy(lon, lat, lon_stem, lat_stem):
     return(np.unravel_index(inds, lon_stem.shape))
     
 def lon_lat_to_cartesian(lon, lat, R = 1):
-    """
-    calculates lon, lat coordinates of a point on a sphere with
-    radius R.  Taken from earthpy (TODO: note URL)
+    """calculates three dimensional cartesian coordinates (x, y, z) for
+    specified longitude, latitude coordinates on a sphere with radius
+    R.  Written and posted at earthpy.org by Oleksandr Huziy.
+    http://earthpy.org/interpolation_between_grids_with_ckdtree.html
+    accessed 19 Mar 2014 by Timothy W. Hilton.
+
     """
     lon_r = np.radians(lon)
     lat_r = np.radians(lat)
@@ -323,8 +301,4 @@ def lon_lat_to_cartesian(lon, lat, R = 1):
     z = R * np.sin(lat_r)
     return x,y,z
 
-if __name__ == "__main__":
-    filename = os.getcwd()+'/CSV/ocs_sgp_aircraft-pfp_1_hats_event.csv'
-
-    #main(filename)
 
