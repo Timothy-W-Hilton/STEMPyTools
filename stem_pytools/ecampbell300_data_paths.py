fb328efaba21bf02f7716a10091db3f1e474a2a1
Timothy W. Hilton <thilton@ucmerced.edu>
2015-03-05 11:56:37 -0800

    factor out check_path_with_message
    - this is useful in many places, so now it gets its own module

--- stem_pytools/ecampbell300_data_paths.py
+++ stem_pytools/ecampbell300_data_paths.py
@@ -3,26 +3,7 @@ define paths to a number of files needed for LRU paper
 """
 
 import os.path
-import sys
-
-
-def check_path_with_msg(this_path):
-    """
-    If the specified path exists on the file system, return True.
-    If it does not exist print a message to stdout and return False.
-    """
-    try:
-        if not(os.path.exists(this_path)):
-            sys.stdout.write('path not found: {}\n'.format(this_path))
-            sys.stdout.flush()
-            return(False)
-        else:
-            return(True)
-    except TypeError:
-        if this_path is None:
-            sys.stdout.write('some paths are None\n')
-            sys.stdout.flush()
-            return(False)
+from stem_pytools.check_paths import check_path_with_msg
 
 
 class paths(object):
@@ -221,6 +202,7 @@ class paths(object):
             'Kettle_Whelan_hybrid_Fsoil',
             'output',
             'AQOUT-124x124-22levs-hybrid_Fsoil_5FebModel.nc')
+
 
 class stemrun(object):
     """
